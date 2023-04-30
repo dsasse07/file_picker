2f48481e19595274e2240b0d3eb54c940f2ae8fd
Danny Sasse <dsasse07@gmail.com>
2023-04-30 00:23:11 -0400

    allow multiple folders

--- app/filePicker.rb
+++ app/filePicker.rb
@@ -1,7 +1,7 @@
 # ENV Variable Config:
 # MNT_DIRECTORY = path to get from this app to /mnt in WSL
 # ex: ../../../../mnt/
-# FILE_DIRECTORY = Path from /mnt to the files
+# FILE_DIRECTORY = Path(s) from /mnt to the files
 # ex: a/videos
 # RUN_DIRECTORY = #Windows path from drive to file's directory. 
 # Ex: C:\\Program Files\\Some Folder\\Example.txt
@@ -10,14 +10,21 @@
 # sudo mkdir MNT_DIRECTORY + drive_letter
 #e ex: A:\\ -> ../../../../mnt/a
 
+# Currently only supports one-level of sub folders from the Drive root
+
 class FilePicker
   include CliControls
 
-  attr_reader :mnt_path, :directory_path, :files_path, :run_path, :files
+  attr_reader :mnt_path, :directory_path, :files_path, :run_path, :files, :select_folder
 
   def initialize
+    @selected_folder = ""
     get_file_paths()
-    load_files()
+    if @files_paths.length > 1
+      select_folder_menu()
+    else
+      load_files(@files_path.first)
+    end
   end
 
   def run
@@ -29,20 +36,28 @@ class FilePicker
 
   def get_file_paths
     @mnt_path=ENV["MNT_DIRECTORY"] # MNT_DIRECTORY is the directory of the /mnt path in WSL of the target drive
-    @directory_path = ENV["FILE_DIRECTORY"]
+    @directory_paths = ENV["FILE_DIRECTORY"]
     @run_path = ENV["RUN_DIRECTORY"]
-    @files_path = @mnt_path+@directory_path
+    @files_paths = @directory_paths.split(",").map{|path| @mnt_path + path } 
   end
 
-  def load_files
+  def load_files(file_path)
     begin
-      @entries =  Dir.entries(@files_path)
+      @entries =  Dir.entries(file_path)
     rescue
       system "sudo mount -t drvfs A: /mnt/a" 
-      @entries =  Dir.entries(@files_path)
+      @entries =  Dir.entries(file_path)
     ensure
-      @files = @entries.select{|f| File.file? File.join(@files_path, f)}
+      @files = @entries.select{|f| File.file? File.join(file_path, f)}
     end
+  end
+
+  def select_folder_menu()
+    map = @files_paths.each_with_object({}){ |path, map| map[path.split("/").last] = path}
+    choices = map.keys
+    @selected_folder = @@prompt.select("Select folder", choices)
+    load_files(map[@selected_folder])
+    main_menu()
   end
 
   def welcome
@@ -58,7 +73,7 @@ class FilePicker
   end
 
   def main_menu
-    choices = ["Open Random", "Pick Five", "Select File", "Exit"]
+    choices = ["Open Random", "Pick Five", "Select File", "Change Folder", "Exit"]
     selection = @@prompt.select("What would you like to do?", choices)
     case selection
       when "Open Random" 
@@ -70,6 +85,8 @@ class FilePicker
       when "Select File"
         select_file()
         main_menu()
+      when "Change Folder"
+        select_folder_menu()
       when "Exit"
         system 'clear'
         exit
@@ -88,7 +105,8 @@ class FilePicker
   end
 
   def open_file( filename )
-    system_path_for_file = @run_path + filename
+    folder_segment = @selected_folder.empty? ? "" : "\\#{@selected_folder}\\"
+    system_path_for_file = @run_path + folder_segment + filename
     system `explorer.exe "#{system_path_for_file}"`
   end
 
